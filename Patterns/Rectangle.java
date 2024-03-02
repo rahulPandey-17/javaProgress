2ba81c3732ead0783a2583a686047b91ec04c37f
rahulPandey-17 <0517rp.rahul@gmail.com>
2024-03-02 13:52:51 +0530

    rectangle

---
+++ Patterns/Rectangle.java
@@ -0,0 +1,12 @@
+// program to print a rectangle on screen
+
+public class Rectangle {
+    public static void main(String... args) {
+        for(int i = 0 ; i < 3 ; i++) {
+            for(int j = 0 ; j < 5 ; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
