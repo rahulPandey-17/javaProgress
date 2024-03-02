20a4dd1c3ec4247be5b53cb8a339a41c96e12c62
rahulPandey-17 <0517rp.rahul@gmail.com>
2024-03-02 19:23:41 +0530

    Right Inverted

---
+++ Patterns/InvertedR.java
@@ -0,0 +1,13 @@
+public class InvertedR {
+    public static void main(String... args) {
+        for(int i = 0 ; i < 4 ; i++) {
+            for(int j = 0 ; j < (4 - i) ; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0 ; j <= i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
