29cdaa05ca5ee8c5bd7f01c8ecafc7ae15f4bdcf
rahulPandey-17 <0517rp.rahul@gmail.com>
2024-03-02 19:00:09 +0530

    Inverted

---
+++ Patterns/Inverted.java
@@ -0,0 +1,12 @@
+// Program to print the inverted pyramid
+
+public class Inverted {
+    public static void main(String... args) {
+        for(int i = 4 ; i > 0 ; i--) {
+            for(int j = 0 ; j < i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
