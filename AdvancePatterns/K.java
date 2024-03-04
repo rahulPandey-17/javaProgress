290baa3f16b92fd62d67d0f217283f0447180a5a
rahulPandey-17 <0517rp.rahul@gmail.com>
2024-03-04 19:57:21 +0530

    k pattern

---
+++ AdvancePatterns/K.java
@@ -0,0 +1,24 @@
+public class K {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = n ; i >= 1 ; i--) {
+            for(int j = 1 ; j <= i ; j++) {
+                System.out.print("*");
+            }
+            for(int j = 1 ; j < (i - n) ; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        for(int i = 1 ; i <= n ; i++) {
+            for(int j = 1 ; j <= i ; j++) {
+                System.out.print("*");
+            }
+            for(int j = 1 ; j < (i - n) ; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
