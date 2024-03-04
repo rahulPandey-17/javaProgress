6624cb075891cf735d95eb98bfa086f89d76f60b
rahulPandey-17 <0517rp.rahul@gmail.com>
2024-03-04 12:44:41 +0530

    Rhombus

---
+++ AdvancePatterns/Rhombus.java
@@ -0,0 +1,16 @@
+// Program to print a solid rhombus
+
+public class Rhombus {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = 1 ; i <= 5 ; i++) {
+            for(int j = 1 ; j <= (n - i) ; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j <= n ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
