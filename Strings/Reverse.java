962f81e1db2c5291d4a5ce8c4d7853a0bd82b057
rahulPandey-17 <0517rp.rahul@gmail.com>
2024-03-07 19:43:54 +0530

    modified string reverse

--- Strings/Reverse.java
+++ Strings/Reverse.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
 public class Reverse {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter te text : ");
+        System.out.print("Enter the text : ");
         StringBuilder str = new StringBuilder(sc.nextLine());
-        StringBuilder rev = new StringBuilder("");
-        for(int i = str.length() - 1 ; i >= 0 ; i--) {
-            rev = rev.append(str.charAt(i));
+        for(int i = 0 ; i < str.length() / 2 ; i++) {
+            int front = i;
+            int back = str.length() - 1 - i;
+
+            char frontChar = str.charAt(front);
+            char backChar = str.charAt(back);
+
+            str.setCharAt(front, backChar);
+            str.setCharAt(back , frontChar);
         }
-        System.out.println("The reversed string is : " + rev);
+        System.out.println("The reversed string is : " + str);
     }
 }
